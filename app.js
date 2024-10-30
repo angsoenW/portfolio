72c0a45036864478dc88ca0f19273e6d2ff396bf
angsoenW <91903037+angsoenW@users.noreply.github.com>
2024-10-29 22:06:22 -0700

    dev

--- app.js
+++ app.js
@@ -14,10 +14,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((req, res, next) =>{
-//     next();
-// })
-
 app.use('/api/v1', apiv1Router);
 
 export default app;
