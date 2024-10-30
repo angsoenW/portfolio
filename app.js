d9b061c738aed42017ec41a29796ecf3db2ccf4b
angsoenW <91903037+angsoenW@users.noreply.github.com>
2024-10-29 21:02:57 -0700

    dev

--- app.js
+++ app.js
@@ -1,34 +1,7 @@
 import express from 'express';
 import path from 'path';
-import cookieParser from 'cookie-parser';
-import logger from 'morgan';
-import sessions from 'express-session';
-import localStorage from "localStorage";
-
-import WebAppAuthProvider from 'msal-node-wrapper'
-
-const authConfig = {
-    auth: {
-        clientId: "8017b637-f037-4234-8f28-14c47c565744",
-        authority: "https://login.microsoftonline.com/f6b6dd5b-f02f-441a-99a0-162ac5060bd2",
-        clientSecret: "A5Z8Q~VMcbCSEeCn~mmj8GViGCHFRqQO2CZpEb_v",
-        redirectUri: "/redirect",
-    },
-    system: {
-        loggerOptions: {
-            loggerCallback(loglevel, message, containsPii) {
-                console.log(message);
-            },
-            piiLoggingEnabled: false,
-            logLevel: 1,
-        }
-    }
-};
 
 import apiv1Router from './routes/api/v1/apiv1.js';
-import models from './models.js';
-
-
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 
@@ -38,59 +11,14 @@ const __dirname = dirname(__filename);
 var app = express();
 
 app.enable('trust proxy')
-
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-const oneDay = 1000 * 60 * 60 * 24
-app.use(sessions({
-    secret: "key",
-    saveUninitialized: true,
-    cookie: {maxAge: oneDay},
-    resave: false
-}))
-
-const authProvider = await WebAppAuthProvider.WebAppAuthProvider.initialize(authConfig);
-app.use(authProvider.authenticate());
-
 app.use((req, res, next) =>{
-    //console.log("session info:", req.session)
     next();
 })
 
-app.use((req, res, next) => {
-    req.models = models;
-    next();
-});
-
 app.use('/api/v1', apiv1Router);
-
-app.get('/redirect', authProvider.authenticate(), (req, res) => {
-    res.redirect('/');
-});
-
-app.get(
-	'/signin',
-	(req, res, next) => {
-		return req.authContext.login({
-			postLoginRedirectUri: "/",
-		})(req, res, next);
-	}
-);
-
-app.get(
-	'/signout',
-	(req, res, next) => {
-        localStorage.clear();
-		return req.authContext.logout({
-			postLogoutRedirectUri: "/",
-		})(req, res, next);
-	}
-);
-
-app.use(authProvider.interactionErrorHandler());
 
 export default app;
--- routes/api/v1/apiv1.js
+++ routes/api/v1/apiv1.js
@@ -1,19 +1,8 @@
 import express from 'express'
 var router = new express.Router()
 
-import generateRouter from './controllers/generateRecipe.js'
-import inventoryRouter from './controllers/inventoryStatus.js'
-import recipeRouter from './controllers/recipe.js'
-import usersRouter from './controllers/users.js'
-import ShelfLifeRouter from './controllers/generateShelfLife.js'
 // import userProfile from './controllers/userProfile.js'
 
-router.use('/generate', generateRouter)
-router.use('/inventory', inventoryRouter)
-router.use('/recipe', recipeRouter)
-router.use('/users', usersRouter)
-router.use('/shelfLife', ShelfLifeRouter)
 // router.use('/userProfile', userProfile)
-
 
 export default router
