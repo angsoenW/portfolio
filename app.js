2a13528b17b5e87091043c375f68a2f1da01e15d
angsoenW <91903037+angsoenW@users.noreply.github.com>
2024-11-12 13:32:09 -0800

    dev

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 import express from 'express';
 import path from 'path';
-
-import apiv1Router from './routes/api/v1/apiv1.js';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+
+import apiv1Router from './routes/api/v1/apiv1.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -12,12 +12,16 @@ var app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) =>{
-    next();
-})
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.use('/api/v1', apiv1Router);
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
 
 export default app;
