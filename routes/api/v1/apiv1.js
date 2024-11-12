85393b6dc712eb64a39976007022a4f7e8bf09ae
angsoenW <91903037+angsoenW@users.noreply.github.com>
2024-11-11 22:25:46 -0800

    changed file source

--- routes/api/v1/apiv1.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-var router = new express.Router()
-
-// import userProfile from './controllers/userProfile.js'
-
-// router.use('/userProfile', userProfile)
-
-export default router
--- src/components/head/head.jsx
+++ src/components/head/head.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Space, Dropdown, Menu } from 'antd';
 import { Link } from 'react-router-dom';
-import './index.css'
+import './index.css';
 
 const baseStyle = {
     width: '100px',
@@ -13,19 +13,11 @@ const baseStyle = {
     justifyContent: 'center',
     fontSize: '18px',
     fontWeight: 'bold',
-    borderRadius: '8px',                  // 圆角
+    borderRadius: '8px',
     border: '2px solid var(--ant-primary-color)',
-    backgroundColor: 'var(--ant-primary-color)',  // 主色
-    boxShadow: '3px 3px 0 rgba(0, 0, 0, 0.2)', // 阴影
-    transition: 'all 0.3s ease',          // 过渡效果
-};
-
-const flexStyle = {
-    width: '100%',
-    height: '60%',
-    display: "flex",
-    flexDirection: 'row',
-    justifyContent: 'space-around',
+    backgroundColor: 'var(--ant-primary-color)',
+    boxShadow: '3px 3px 0 rgba(0, 0, 0, 0.2)',
+    transition: 'all 0.3s ease',
 };
 
 const workMenu = (
@@ -38,26 +30,20 @@ const workMenu = (
 
 const Head = () => {
     return (
-        <div style={{ height: "20%" }}>
-            <Space className="head" direction="vertical" align="center" style={flexStyle}>
-                <div style={{
-                    fontSize: '24px',
-                    fontWeight: 'bold',
-                    color: 'var(--ant-color-link)',
-                    textShadow: '2px 2px rgba(0, 0, 0, 0.2)',
-                    paddingRight: "100px"
-                }} className="name">
+        <div className="head">
+            <Space className="head-content" direction="vertical" align="center">
+                <div className="name">
                     Shuoheng Wang
                 </div>
 
                 <div className="navbar">
-                    <Space size="middle" style={{ margin: '20px 0' }}>
+                    <Space size="middle">
                         <Dropdown overlay={workMenu} placement="bottom">
                             <div style={{ ...baseStyle, cursor: 'pointer' }}>
                                 Work
                             </div>
                         </Dropdown>
-                        {[ 'About', 'Resume' ].map((text, i) => (
+                        {['About', 'Resume'].map((text, i) => (
                             <Link to={`/${text.toLowerCase()}`} key={i}>
                                 <div style={{
                                     ...baseStyle,
